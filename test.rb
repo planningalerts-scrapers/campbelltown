4725ac651623ad69853710e3631d15981cd94e60
Matthew Landauer <mlandauer@gmail.com>
2019-05-14 17:16:12 +1000

    Added regression test

---
+++ test.rb
@@ -0,0 +1,24 @@
+# A simple regression test
+# Simulates a fixed external website using data from fixtures
+# Checks that the data is as expected
+
+require 'vcr'
+require 'scraperwiki'
+require 'yaml'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock
+end
+
+File.delete("./data.sqlite") if File.exist?("./data.sqlite")
+
+VCR.use_cassette("scraper") do
+  require "./scraper"
+end
+
+expected = YAML.load(File.read("fixtures/expected.yml"))
+results = ScraperWiki.select("* from data")
+
+raise "Failed" unless results == expected
+puts "Succeeded"
