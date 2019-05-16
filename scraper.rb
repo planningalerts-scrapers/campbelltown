ab0a6e2d0a5f0794d281e55f90268751d4b5bc61
Matthew Landauer <mlandauer@gmail.com>
2019-05-16 12:31:03 +1000

    Use extract_table_data

--- scraper.rb
+++ scraper.rb
@@ -4,23 +4,6 @@ require 'date'
 INIT_URL = "https://ebiz.campbelltown.nsw.gov.au/ePathway/Production/Web/GeneralEnquiry/ExternalRequestBroker.aspx?Module=EGELAP&Class=0PEAPP&Type=DATRAC"
 INFO_URL = "https://ebiz.campbelltown.nsw.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryLists.aspx?ModuleCode=LAP"
 
-def scrape_result_row(result_row)
-  fields = result_row.search('td').map { |f| f.inner_text }
-
-  record = {
-    'council_reference' => fields[0],
-    'address' => fields[4],
-    'description' => fields[1],
-    'date_received' => Date.strptime(fields[2], '%d/%m/%Y').to_s,
-    'date_scraped' => Date.today.to_s,
-    'info_url' => INFO_URL
-  }
-  # on_notice_from and on_notice_to don't seem to be available for this council.
-  # puts record
-  puts "Saving record " + record["council_reference"] + " - " + record['address']
-  ScraperWiki.save_sqlite(['council_reference'], record)
-end
-
 scraper = EpathwayScraper.new(base_url: INIT_URL)
 
 agent = scraper.agent
@@ -30,8 +13,21 @@ current_page = scraper.click_search_on_form(current_page.form)
 current_page_index = 1
 
 loop do
-  current_page.search('tr.ContentPanel, tr.AlternateContentPanel').each do |tr|
-    scrape_result_row(tr)
+  table = current_page.at("table.ContentPanel")
+  scraper.extract_table_data(table).each do |row|
+    record = {
+      'council_reference' => row["Application Number"],
+      'address' => row["Location Address"],
+      # TODO: Change this to using "Description"
+      'description' => row["Application Type"],
+      'date_received' => Date.strptime(row["Date Lodged"], '%d/%m/%Y').to_s,
+      'date_scraped' => Date.today.to_s,
+      'info_url' => INFO_URL
+    }
+    # on_notice_from and on_notice_to don't seem to be available for this council.
+    # puts record
+    puts "Saving record " + record["council_reference"] + " - " + record['address']
+    ScraperWiki.save_sqlite(['council_reference'], record)
   end
 
   next_link = current_page.links_with(:text => (current_page_index+1).to_s)[0]
