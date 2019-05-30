628aff25ec04254d3700b47eb19b814291593a97
Matthew Landauer <mlandauer@gmail.com>
2019-05-30 10:15:33 +1000

    Upgrade to latest version of gem

--- scraper.rb
+++ scraper.rb
@@ -1,8 +1,6 @@
 require 'epathway_scraper'
 
-scraper = EpathwayScraper::Scraper.new(
-  base_url: "https://ebiz.campbelltown.nsw.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryLists.aspx?ModuleCode=LAP",
-  index: 2
+EpathwayScraper::Scraper.scrape_and_save(
+  "https://ebiz.campbelltown.nsw.gov.au/ePathway/Production",
+  list_type: :all
 )
-
-scraper.scrape_and_save
