39e92fcd0a5169d69126fd9e30d545ff8521d227
Matthew Landauer <mlandauer@gmail.com>
2019-06-01 05:14:40 +1000

    Upgrade to latest gem

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,6 @@
 require 'epathway_scraper'
 
-EpathwayScraper::Scraper.scrape_and_save(
+EpathwayScraper.scrape_and_save(
   "https://ebiz.campbelltown.nsw.gov.au/ePathway/Production",
   list_type: :all
 )
